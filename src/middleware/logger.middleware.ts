f7530c5548d825ec15aa48a642e28dda45ba884a
Mattias Söderberg <mattes.email@gmail.com>
2023-09-07 14:49:39 +0200

    Remove prisma code

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -5,11 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
-  private prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  });
   use(req: Request, res: Response, next: NextFunction) {
-    // this.logger.log(req.method, req.url, res.statusCode);
     this.logger.log(
       `Logging ${req.method} ${req.baseUrl} status ${res.statusCode}`,
     );
