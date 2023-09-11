d6471581edba185339de5b0d99daa54e6ee566fe
Mattias Söderberg <mattes.email@gmail.com>
2023-09-11 15:50:10 +0200

    Add exception logger filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
 import { SchedulesModule } from './schedules/schedules.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { APP_FILTER } from '@nestjs/core';
+import { ExceptionLoggerFilter } from './filters/exception-logger.filters';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: ExceptionLoggerFilter },
+  ],
   imports: [UsersModule, EventsModule, SchedulesModule],
 })
 export class AppModule implements NestModule {
