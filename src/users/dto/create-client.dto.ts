f2e30b865c5758eaf5862fee3534ab7df618b1b5
Mattias Söderberg <mattes.email@gmail.com>
2023-09-11 12:59:11 +0200

    Add create client dto

--- src/users/dto/create-client.dto.ts
+++ src/users/dto/create-client.dto.ts
@@ -1,18 +1,62 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  ArrayContains,
+  IsArray,
+  IsDate,
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+
+class Asset {
+  @IsUUID()
+  assetId: string;
+}
+
+class Relative {
+  @IsUUID()
+  userId: string;
+}
 
 export class CreateClientDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
 
   @ApiProperty()
+  @IsDateString()
   bornAt: Date;
 
   @ApiProperty()
-  assets: string[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Asset)
+  assets: Asset[];
 
   @ApiProperty({ required: false })
-  schedules: string[];
+  @IsOptional()
+  @IsUUID()
+  user: { uuid: string };
+
+  @ApiProperty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Relative)
+  relatives: Relative[];
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  note: string;
 }
