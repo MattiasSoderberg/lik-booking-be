6c6b3d30d6538c540d1a5a60e16d5ef2c0dc44e8
Mattias Söderberg <mattes.email@gmail.com>
2023-09-11 12:58:21 +0200

    Remove unused code

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude, Type } from 'class-transformer';
+import { Type } from 'class-transformer';
 import {
   IsNotEmpty,
   IsEmail,
@@ -49,7 +49,6 @@ export class CreateUserDto {
 
   @ApiProperty()
   @IsNotEmpty()
-  // @Exclude()
   password: string;
 
   @ApiProperty({ required: false })
