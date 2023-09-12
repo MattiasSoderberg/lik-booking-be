b76dc99340054089146f523d5f1290386c3b2cad
Mattias Söderberg <mattes.email@gmail.com>
2023-09-12 08:25:55 +0200

    Add Public decorator to login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './sign-in.dto';
+import { Public } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @Public()
   logIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
