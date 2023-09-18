4a785df9972cd9e01a61ca2f1a7461cd5b5fd041
Mattias Söderberg <mattes.email@gmail.com>
2023-09-18 17:45:35 +0200

    Add permission checks

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,21 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { hash } from 'bcrypt';
-import { Prisma } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { PrismaErrors } from 'src/utils/prisma-errors.enum';
 import { UserExists } from './exceptions/user-exists.exception';
+import { RoleEnum } from 'src/utils/role.enum';
+import { AppAbility } from 'src/auth/auth.ability';
+import { accessibleBy } from '@casl/prisma';
+import { Action } from 'src/utils/action.enum';
 
 @Injectable()
 export class UsersService {
@@ -59,14 +68,22 @@ export class UsersService {
     return this.prisma.client.findUniqueOrThrow({ where: { uuid } });
   }
 
-  async createUser(createUserDto: CreateUserDto) {
-    const { address, ...userdata } = createUserDto;
+  async createUser(createUserDto: CreateUserDto, ability) {
+    const { address, role, ...userdata } = createUserDto;
     const hashedPassword = await hash(userdata.password, 10);
     userdata.password = hashedPassword;
 
+    if (!ability.can(Action.Manage, 'all')) {
+      throw new ForbiddenException('Admin route');
+    }
+
     try {
       const user = await this.prisma.user.create({
-        data: { ...userdata, address: { create: address } },
+        data: {
+          ...userdata,
+          address: { create: address },
+          role: { connect: { id: RoleEnum[role] } },
+        },
         include: { address: true },
       });
 
@@ -84,41 +101,69 @@ export class UsersService {
     }
   }
 
-  findAllUsers() {
-    return this.prisma.user.findMany({ include: { address: true } });
+  findMe(ability: AppAbility, user: User) {
+    return user;
   }
 
-  findOneUser(uuid: string) {
-    return this.prisma.user.findUniqueOrThrow({
-      where: { uuid },
-      include: { address: true },
-    });
+  findAllUsers(ability: AppAbility) {
+    try {
+      return this.prisma.user.findMany({ where: accessibleBy(ability).User });
+    } catch (error) {
+      console.log('FIND ALL ERROR', error);
+    }
+  }
+
+  findOneUser(uuid: string, ability: AppAbility, user: User) {
+    try {
+      return this.prisma.user.findFirst({
+        where: {
+          AND: [accessibleBy(ability).User, { uuid }],
+        },
+      });
+    } catch (error) {
+      throw new ForbiddenException(error);
+    }
   }
 
   async findOneUserByEmail(email: string) {
     return await this.prisma.user.findUnique({ where: { email: email } });
   }
 
-  async updateUser(uuid: string, updateUserDto: UpdateUserDto) {
-    const { address, ...userData } = updateUserDto;
-    const user = await this.prisma.user.update({
-      where: { uuid },
-      data: {
-        ...userData,
-        address: {
-          upsert: {
-            where: { uuid },
-            create: address,
-            update: address,
-          },
+  updateUser(uuid: string, updateUserDto: UpdateUserDto) {
+    const { address, role, ...userData } = updateUserDto;
+
+    const dataToUpdate = {
+      ...userData,
+      role: { connect: { id: RoleEnum[role] } },
+    };
+
+    if (address) {
+      dataToUpdate['address'] = {
+        upsert: {
+          where: { uuid },
+          create: address,
+          update: address,
         },
-      },
-      include: { address: true },
-    });
-    return user;
+      };
+    }
+
+    return this.prisma.user.update({ where: { uuid }, data: dataToUpdate });
   }
 
   removeUser(uuid: string) {
     return `This action removes a #${uuid} user`;
+  }
+
+  async getUserWPermissions(uuid: string) {
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: { uuid },
+        include: { role: { include: { permissions: true } } },
+      });
+
+      return user;
+    } catch (error) {
+      throw new NotFoundException(`User with uuid ${uuid} not found.`);
+    }
   }
 }
