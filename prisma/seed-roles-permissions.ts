7acbe82534674b758a4c247e2f4dbfb2164ff2a6
Mattias Söderberg <mattes.email@gmail.com>
2023-09-19 14:47:18 +0200

    Add users

--- prisma/seed-roles-permissions.ts
+++ prisma/seed-roles-permissions.ts
@@ -90,12 +90,56 @@ export const permissions = [
   },
 ];
 
+const users = [
+  {
+    email: 'admin@lik.com',
+    firstName: 'Matte',
+    lastName: 'admin',
+    password: 'password',
+    role: 1,
+    phoneNumber: '070-1234567',
+    address: {
+      street: 'street',
+      zipCode: '12345',
+      area: 'stockholm',
+    },
+  },
+  {
+    email: 'staff@lik.com',
+    firstName: 'Matte',
+    lastName: 'staff',
+    password: 'password',
+    role: 2,
+    phoneNumber: '070-1234567',
+    address: {
+      street: 'street',
+      zipCode: '12345',
+      area: 'stockholm',
+    },
+  },
+  {
+    email: 'relative@lik.com',
+    firstName: 'Matte',
+    lastName: 'relative',
+    password: 'password',
+    role: 3,
+    phoneNumber: '070-1234567',
+    address: {
+      street: 'street',
+      zipCode: '12345',
+      area: 'stockholm',
+    },
+  },
+];
+
 const prisma = new PrismaClient();
 const logger = new Logger('SEED ROLES AND PERMISSIONS');
 
 async function main() {
   let roleCount = 0;
   let permissionCount = 0;
+  let userCount = 0;
+
   for await (const role of roles) {
     const result = await prisma.role.upsert({
       where: { id: role.id },
@@ -117,13 +161,35 @@ async function main() {
       permissionCount++;
     }
   }
-  return { roleCount, permissionCount };
+
+  for await (const user of users) {
+    const { address, role, ...userData } = user;
+    const dataToCreate = {
+      ...userData,
+      role: { connect: { id: role } },
+    };
+    if (address) {
+      const { street, zipCode, area } = address;
+      dataToCreate['address'] = {
+        connectOrCreate: {
+          where: { address_identifier: { street, zipCode, area } },
+          create: address,
+        },
+      };
+    }
+    const result = await prisma.user.create({ data: dataToCreate });
+    if (result) {
+      userCount++;
+    }
+  }
+  return { roleCount, permissionCount, userCount };
 }
 
 main()
   .then(async (res) => {
     logger.log(`Roles created: ${res.roleCount}`);
     logger.log(`Permissions created: ${res.permissionCount}`);
+    logger.log(`Users created: ${res.userCount}`);
     await prisma.$disconnect();
   })
   .catch(async (error) => {
