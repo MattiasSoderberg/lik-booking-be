b9a8f38948beb4256480a51465d34cfa098b7242
Mattias Söderberg <mattes.email@gmail.com>
2023-09-28 15:43:27 +0200

    Enabled global pipes

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
 
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //     transform: true,
-  //   }),
-  // );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      // transform: true,
+    }),
+  );
   // app.useGlobalInterceptors(new TransformDataInterceptor(UserResponseDto));
 
   const config = new DocumentBuilder()
