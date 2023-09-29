fce564ca1bd1c2a271bc2ea5f6b1f2de82ef31e5
Mattias Söderberg <mattes.email@gmail.com>
2023-09-29 11:41:36 +0200

    Add dto

--- src/schedules/dto/create-semester.dto.ts
+++ src/schedules/dto/create-semester.dto.ts
@@ -1,1 +1,21 @@
-export class CreateSemesterDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsEnum } from 'class-validator';
+
+enum SemesterPeriod {
+  Spring = 'SPRING',
+  Fall = 'FALL',
+}
+
+export class CreateSemesterDto {
+  @ApiProperty()
+  @IsDateString()
+  startAt: Date;
+
+  @ApiProperty()
+  @IsDateString()
+  endAt: Date;
+
+  @ApiProperty({ enum: SemesterPeriod })
+  @IsEnum(SemesterPeriod)
+  period: SemesterPeriod;
+}
