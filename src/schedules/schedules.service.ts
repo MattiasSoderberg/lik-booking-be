0159b0304ef2515882dea9a69ac839f1a7ae8bc0
Mattias Söderberg <mattes.email@gmail.com>
2023-09-29 11:42:44 +0200

    Add logic for get, post and delete semester

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateSemesterDto } from './dto/create-semester.dto';
+import { AppAbility } from 'src/auth/auth.ability';
+import { Action } from 'src/utils/action.enum';
+import { AdminRouteException } from 'src/auth/exceptions/admin-route.exception';
+import { Prisma } from '@prisma/client';
+import { PrismaErrors } from 'src/utils/prisma-errors.enum';
+import { accessibleBy } from '@casl/prisma';
 
 @Injectable()
 export class SchedulesService {
   constructor(private prisma: PrismaService) {}
-
-  createSemester(createSemesterDto: CreateSemesterDto) {
-    return 'Create semester';
-  }
-
-  findAllSemesters() {
-    return this.prisma.semester.findMany({});
-  }
 
   create(createScheduleDto: CreateScheduleDto) {
     return 'This action adds a new schedule';
@@ -34,5 +36,48 @@ export class SchedulesService {
 
   remove(uuid: string) {
     return `This action removes a #${uuid} schedule`;
+  }
+
+  async createSemester(
+    createSemesterDto: CreateSemesterDto,
+    ability: AppAbility,
+  ) {
+    if (!ability.can(Action.Create, 'Semester')) {
+      throw new AdminRouteException();
+    }
+
+    const date = new Date(createSemesterDto.startAt);
+    const year = date.getFullYear();
+
+    try {
+      return await this.prisma.semester.create({
+        data: { ...createSemesterDto, year },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error?.code === PrismaErrors.UniqueConstraintError
+      ) {
+        throw new BadRequestException(
+          `Period already exists on year: ${year}.`,
+        );
+      }
+      throw new InternalServerErrorException(
+        'Something went wrong when creating semester.',
+      );
+    }
+  }
+
+  async findAllSemesters(ability: AppAbility) {
+    return await this.prisma.semester.findMany({
+      where: accessibleBy(ability).Semester,
+    });
+  }
+
+  async removeSemester(uuid: string, ability: AppAbility) {
+    if (!ability.can(Action.Delete, 'Semester')) {
+      throw new AdminRouteException();
+    }
+    return await this.prisma.semester.delete({ where: { uuid } });
   }
 }
