9e942ee4867d0b3098c5ebfb30d73232daad6417
Mattias Söderberg <mattes.email@gmail.com>
2023-10-03 13:46:54 +0200

    Add create event dto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,54 @@
-export class CreateEventDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDateString,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class RelationEntity {
+  @IsUUID()
+  uuid: string;
+}
+
+export class CreateEventDto {
+  @ApiProperty()
+  @IsDateString()
+  startAt: Date;
+
+  @ApiProperty()
+  @IsDateString()
+  endAt: Date;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  note: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  isActive: boolean;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  isBlocking: boolean;
+
+  @ApiProperty()
+  @IsOptional()
+  @Type(() => RelationEntity)
+  assetId: RelationEntity;
+
+  @ApiProperty()
+  @IsOptional()
+  @Type(() => RelationEntity)
+  staffId: RelationEntity;
+
+  @ApiProperty()
+  @IsOptional()
+  @Type(() => RelationEntity)
+  clientId: RelationEntity;
+}
