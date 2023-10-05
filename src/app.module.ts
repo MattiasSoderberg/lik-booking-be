b79494d34171391d2f8656a0d91911c5077ffdd1
Mattias Söderberg <mattes.email@gmail.com>
2023-10-05 11:22:28 +0200

    Remove schedule resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
-import { SchedulesModule } from './schedules/schedules.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { ExceptionLoggerFilter } from './filters/exception-logger.filters';
 import { AuthModule } from './auth/auth.module';
 import { AuthGuard } from './auth/auth.guard';
+import { SemestersModule } from './semesters/semesters.module';
 
 @Module({
   controllers: [AppController],
@@ -17,7 +17,7 @@ import { AuthGuard } from './auth/auth.guard';
     // { provide: APP_FILTER, useClass: ExceptionLoggerFilter },
     { provide: APP_GUARD, useClass: AuthGuard },
   ],
-  imports: [UsersModule, EventsModule, SchedulesModule, AuthModule],
+  imports: [UsersModule, EventsModule, AuthModule, SemestersModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
