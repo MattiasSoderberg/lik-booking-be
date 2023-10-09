5c91239e03c7df42fb4eacadc303806a879442ed
Mattias Söderberg <mattes.email@gmail.com>
2023-10-09 15:14:23 +0200

    Add import EventsModule

--- src/semesters/semesters.module.ts
+++ src/semesters/semesters.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SemestersService } from './semesters.service';
 import { SemestersController } from './semesters.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
   controllers: [SemestersController],
   providers: [SemestersService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, EventsModule],
 })
 export class SemestersModule {}
