20820ec42a521162088329951de71751a6de8475
Mattias Söderberg <mattes.email@gmail.com>
2023-10-13 09:30:37 +0200

    Add schedule task service

--- src/semesters/semesters.module.ts
+++ src/semesters/semesters.module.ts
@@ -4,10 +4,11 @@ import { SemestersController } from './semesters.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EventsModule } from 'src/events/events.module';
 import { SchedulesService } from './schedules.service';
+import { ScheduleTaskService } from './schedule-task.service';
 
 @Module({
   controllers: [SemestersController],
-  providers: [SemestersService, SchedulesService],
+  providers: [SemestersService, SchedulesService, ScheduleTaskService],
   imports: [PrismaModule, EventsModule],
 })
 export class SemestersModule {}
