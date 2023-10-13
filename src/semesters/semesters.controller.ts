df45bc9d0bedd61afe95aad4c4fb0ca53a1a5642
Mattias Söderberg <mattes.email@gmail.com>
2023-10-13 09:14:03 +0200

    Add schedule routes and service

--- src/semesters/semesters.controller.ts
+++ src/semesters/semesters.controller.ts
@@ -15,15 +15,22 @@ import { DatePeriodValidationPipe } from './pipes/date-period-validation.pipe';
 import { DateOverlapValidationPipe } from './pipes/date-overlap-validation.pipe';
 import { AuthenticatedRequest } from 'src/auth/auth.interface';
 import { ApiTags } from '@nestjs/swagger';
+import { SchedulesService } from './schedules.service';
+import { CreateScheduleDto } from './dto/create-schedule.dto';
+import { ScheduleValidationPipe } from './pipes/schedule-validation.pipe';
 
 @ApiTags('Semesters')
 @Controller('semesters')
 export class SemestersController {
-  constructor(private readonly semestersService: SemestersService) {}
+  constructor(
+    private readonly semestersService: SemestersService,
+    private readonly schedulesService: SchedulesService,
+  ) {}
 
   @Post()
   create(
-    @Body(DatePeriodValidationPipe, DateOverlapValidationPipe)
+    // @Body(DatePeriodValidationPipe, DateOverlapValidationPipe)
+    @Body(DatePeriodValidationPipe)
     createSemesterDto: CreateSemesterDto,
     @Request() req: AuthenticatedRequest,
   ) {
@@ -37,22 +44,55 @@ export class SemestersController {
     return this.semestersService.findAll(ability);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.semestersService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.semestersService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateSemesterDto: UpdateSemesterDto,
-  ) {
-    return this.semestersService.update(+id, updateSemesterDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateSemesterDto: UpdateSemesterDto,
+  // ) {
+  //   return this.semestersService.update(+id, updateSemesterDto);
+  // }
 
   @Delete(':uuid')
   remove(@Param('uuid') uuid: string, @Request() req: AuthenticatedRequest) {
     const { ability } = req;
     return this.semestersService.remove(uuid, ability);
+  }
+
+  @Delete('schedules/:uuid')
+  removeSchedule(
+    @Param('uuid') uuid: string,
+    @Request() req: AuthenticatedRequest,
+  ) {
+    const { ability } = req;
+    return this.schedulesService.remove(uuid, ability);
+  }
+
+  @Post(':semesterUuid/schedules')
+  createSchedule(
+    @Param('semesterUuid') semesterUuid: string,
+    @Body(ScheduleValidationPipe) createScheduleDto: CreateScheduleDto,
+    @Request() req: AuthenticatedRequest,
+  ) {
+    const { ability, user } = req;
+    return this.schedulesService.create(
+      semesterUuid,
+      createScheduleDto,
+      ability,
+      user,
+    );
+  }
+
+  @Get(':semesterUuid/schedules')
+  findAllSchedules(
+    @Param('semesterUuid') semesterUuid: string,
+    @Request() req: AuthenticatedRequest,
+  ) {
+    const { ability } = req;
+    return this.schedulesService.findAllBySemester(semesterUuid, ability);
   }
 }
