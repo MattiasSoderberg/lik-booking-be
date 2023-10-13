2ae30dd5511e437d0ec036e746e138b0340435ed
Mattias Söderberg <mattes.email@gmail.com>
2023-10-13 09:09:14 +0200

    Add schedule service

--- src/semesters/semesters.module.ts
+++ src/semesters/semesters.module.ts
@@ -3,10 +3,11 @@ import { SemestersService } from './semesters.service';
 import { SemestersController } from './semesters.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EventsModule } from 'src/events/events.module';
+import { SchedulesService } from './schedules.service';
 
 @Module({
   controllers: [SemestersController],
-  providers: [SemestersService],
+  providers: [SemestersService, SchedulesService],
   imports: [PrismaModule, EventsModule],
 })
 export class SemestersModule {}
