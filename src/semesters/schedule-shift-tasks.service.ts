3a8dc0177a806795177d8ee587086ee47452d282
Mattias Söderberg <mattes.email@gmail.com>
2023-10-17 17:21:52 +0200

    Add validation method to create

--- src/semesters/schedule-shift-tasks.service.ts
+++ src/semesters/schedule-shift-tasks.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateScheduleShiftTaskDto } from './dto/create-schedule-shift-task.dto';
 import { AppAbility } from 'src/auth/auth.ability';
 import { UpdateScheduleShiftTaskDto } from './dto/update-schedule-shift-task.dto';
+import { Action } from 'src/utils/action.enum';
+import { AdminRouteException } from 'src/auth/exceptions/admin-route.exception';
 
 @Injectable()
 export class ScheduleShiftTasksService {
@@ -15,7 +17,24 @@ export class ScheduleShiftTasksService {
     // TODO FIX USER TYPE
     user,
   ) {
-    return 'Creating schedule shift task';
+    if (!ability.can(Action.Create, 'ScheduleShiftTask')) {
+      throw new AdminRouteException();
+    }
+    try {
+      await this.taskWithinShiftValidation(createScheduleShiftTaskDto, uuid);
+      const dataToCreate = {
+        ...createScheduleShiftTaskDto,
+        scheduleShift: {
+          connect: { uuid },
+        },
+      };
+
+      return await this.prisma.scheduleShiftTask.create({ data: dataToCreate });
+    } catch (error) {
+      if (error?.response) {
+        throw new BadRequestException(error.response);
+      }
+    }
   }
 
   async update(
@@ -31,4 +50,20 @@ export class ScheduleShiftTasksService {
   async findAllBySchedule(uuid: string, ability: AppAbility) {}
 
   async remove(uuid: string, ability: AppAbility) {}
+
+  private async taskWithinShiftValidation(
+    task: CreateScheduleShiftTaskDto,
+    scheduleShiftUuid: string,
+  ) {
+    const shift = await this.prisma.scheduleShift.findFirst({
+      where: { uuid: scheduleShiftUuid },
+    });
+    const taskStartAt = new Date(task.startAt);
+    const taskEndAt = new Date(task.endAt);
+
+    if (taskStartAt < shift.startAt || taskEndAt > shift.endAt) {
+      throw new BadRequestException('Task date must be within shift date');
+    }
+    // return true;
+  }
 }
