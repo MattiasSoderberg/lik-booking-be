73416af2254f9d8988abc5f3842bc28cb3d70766
Mattias Söderberg <mattes.email@gmail.com>
2023-10-25 15:20:07 +0200

    Add weekSchedule class and property

--- src/semesters/dto/create-schedule.dto.ts
+++ src/semesters/dto/create-schedule.dto.ts
@@ -1,7 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
   IsEnum,
+  IsMilitaryTime,
   IsObject,
   IsOptional,
   IsUUID,
@@ -14,9 +18,30 @@ enum SemesterPlan {
   STAFF = 'STAFF',
 }
 
+enum Weekday {
+  MONDAY = 'MONDAY',
+  TUESDAY = 'TUESDAY',
+  WEDNESDAY = 'WEDNESDAY',
+  THURSDAY = 'THURSDAY',
+  FRIDAY = 'FRIDAY',
+  SATURDAY = 'SATURDAY',
+  SUNDAY = 'SUNDAY',
+}
+
 class RelationEntity {
   @IsUUID()
   uuid: string;
+}
+
+export class WeekScheduleShift {
+  @IsEnum(Weekday)
+  weekday: Weekday;
+
+  @IsMilitaryTime()
+  startAt: string;
+
+  @IsMilitaryTime()
+  endAt: string;
 }
 
 export class CreateScheduleDto {
@@ -68,4 +93,26 @@ export class CreateScheduleDto {
   @IsObject({ each: true })
   @Type(() => RelationEntity)
   eventGroup?: RelationEntity;
+
+  @ApiProperty()
+  @IsOptional()
+  @ArrayMinSize(1)
+  @ArrayMaxSize(7)
+  @ValidateNested({
+    message:
+      ' must be an array of objects like: {weekday: "MONDAY", startAt: "08:00", endAt: "17:00"}',
+  })
+  @Type(() => WeekScheduleShift)
+  weekSchedule?: WeekScheduleShift[];
+
+  @ApiProperty()
+  @IsOptional()
+  @ArrayMinSize(1)
+  @ArrayMaxSize(7)
+  @ValidateNested({
+    message:
+      ' must be an array of objects like: {weekday: "MONDAY", startAt: "08:00", endAt: "17:00"}',
+  })
+  @Type(() => WeekScheduleShift)
+  alternativeWeekSchedule?: WeekScheduleShift[];
 }
